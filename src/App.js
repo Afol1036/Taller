15d255cd6c74e7f5a023d44e590fc94f9b011169
Afol1036 <139852090+Afol1036@users.noreply.github.com>
2024-10-11 15:42:27 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+function Saludar(props) { 
+  const name = props.name;
+  const idioma = props.idioma;
+
+  const saludo = idioma === 'es' ? 'Hola' : idioma === 'en' ? 'Hello' : 'Salut'; 
+  
+  return (
+    <div>
+      <h1>{saludo}, {name}!</h1>
+    </div>
+  );
+}
+
+
+function Contenedor(props) {
+  return (
+    <div style={{ border: '2px solid black', padding: '10px' }}>
+      {props.children}
+    </div>
+  );
+}
+
+
+function Contador() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      <p>Haz presionado  {count} veces</p>
+      <button onClick={() => setCount(count + 1)}>
+        Presioname
+      </button>
+    </div>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Contenedor >
+      
+      <Saludar name="María" idioma="en" />
+     
+      <Saludar name="Pedro" idioma="es" />
+     
+     <Contador/>
+    </Contenedor>
   );
 }
 
